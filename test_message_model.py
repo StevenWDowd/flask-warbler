feac586338efb9e4fdb970e0b815a98cbf2c5178
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-09-08 11:21:29 -0700

    tests for message model

--- test_message_model.py
+++ test_message_model.py
@@ -29,3 +29,72 @@ db.drop_all()
 db.create_all()
 
 class MessageModelTestCase(TestCase):
+
+    def setUp(self):
+        Message.query.delete()
+        User.query.delete()
+
+        u1 = User.signup("u1", "u1@email.com", "password", None)
+        u2 = User.signup("u2", "u2@email.com", "password", None)
+
+        msg1 = Message(text="test text 1")
+        u1.messages.append(msg1)
+
+        msg2 = Message(text="test text 2")
+        u2.messages.append(msg2)
+
+        db.session.commit()
+
+        self.u1_id = u1.id
+        self.u2_id = u2.id
+        self.msg1_id = msg1.id
+        self.msg2_id = msg2.id
+
+    def tearDown(self):
+        db.session.rollback()
+
+    def test_message_is_created(self):
+        """ Tests the attributes of created messages"""
+
+        msg1 = Message.query.get(self.msg1_id)
+        u1 = User.query.get(self.u1_id)
+
+        self.assertEqual(msg1.user_id, u1.id)
+        self.assertEqual(msg1.text, "test text 1")
+
+    def test_invalid_text_in_message(self):
+        """Test for invalid inputs to message text """
+
+        u1 = User.query.get(self.u1_id)
+
+        with self.assertRaises(TypeError):
+            msg3 = Message(3)
+            u1.messages.append(msg3)
+            db.session.commit()
+
+    def test_user_for_their_messages(self):
+        """Tests a user for their messages"""
+        u1 = User.query.get(self.u1_id)
+        msg3 = Message(text="test text 3")
+        u1.messages.append(msg3)
+        db.session.commit()
+
+        self.assertEqual(u1.messages[0].id, self.msg1_id)
+        self.assertEqual(len(u1.messages), 2)
+
+    def test_user_for_invalid_messages(self):
+        """Test a user does not have messages by another user"""
+
+        u1
+
+#tests:
+    #if message is assocated with creator (user.messages)
+    #match Messsage.user_id to user.id
+
+    #if a user's message is associated with them
+
+    #if we add a new message its output is as expected
+
+    #test if we can create an empty message
+
+
